b2a1d36364f40381cb9e648535858fd84f6702dd
LiorTur1991 <59708883+LiorTur1991@users.noreply.github.com>
2021-05-11 16:51:34 +0300

    Initial commit

---
+++ CostItemManager.js
@@ -0,0 +1,23 @@
+window.todo = {};
+
+window.todo.CostItem = function(date ,description,note){
+    let costItem;
+    costItem.Date = date;
+    costItem.description = description;
+    costItem.note = note;
+    costItem.name = "SAda";
+    return costItem;
+}
+
+window.todo.addItem = function(ob) {
+    var data = todo.getItems();
+    if(data == null)
+        data = [];
+    data.push(ob)
+    let dataString = JSON.stringify(data);
+    localStorage.setItem("data", dataString);
+};
+window.todo.getItems = function() {
+    var data = localStorage.getItem("data");
+    if(data) return JSON.parse(data);
+}
